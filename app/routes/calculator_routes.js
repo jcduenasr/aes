56d9f9382e63652406ea938f43b7a127722aae55
Juan C Dueñas R <jeancarlo47@gmail.com>
2018-11-05 20:42:32 -0500

    ajustes recursos suma y multiplicación

--- app/routes/calculator_routes.js
+++ app/routes/calculator_routes.js
@@ -1,8 +1,10 @@
 module.exports = function(app) {
-    app.get('/sum/:num1/:num2', (req, res) => {
-        const num1 = req.params.num1;
-        const num2 = req.params.num2;
-        var result = num1 * num2
+    app.get(/^\/sum\/(.+)/, (req, res) => {
+        var result = 0;
+        var numbersArray = req.params[0].split("/")
+        for (var current in numbersArray) {
+            result = result + Number(numbersArray[current])
+        }
         res.send(`Result: ${result}`)
     });
 
@@ -13,10 +15,12 @@ module.exports = function(app) {
         res.send(`Result: ${result}`)
     });
 
-    app.get('/mul/:num1/:num2', (req, res) => {
-        const num1 = req.params.num1;
-        const num2 = req.params.num2;
-        var result = num1 * num2
+    app.get(/^\/mul\/(.+)/, (req, res) => {
+        var result = 1;
+        var numbersArray = req.params[0].split("/").filter(val => val)
+        for (var current in numbersArray) {
+            result = result * Number(numbersArray[current])
+        }
         res.send(`Result: ${result}`)
     });
 
